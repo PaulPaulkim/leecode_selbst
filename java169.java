b6a3aab00cb17b4f9cc8c4802b894599174838eb
paul kim <kth5009@gmail.com>
2025-08-28 12:03:12 +0200

    169. Majority Element

--- java169.java
+++ java169.java
@@ -1,3 +1,38 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class java169 {
-    
+    static class Solution{
+        public int majorityElement(int[] nums){
+            Map<Integer , Integer> countMap = new HashMap<>();
+            
+            for(int num : nums){
+                countMap.put(num, countMap.getOrDefault(num,0)+1);
+            }
+
+            int majority= -1;
+            int maxCount = 0;
+
+            for(Map.Entry<Integer, Integer> entry : countMap.entrySet() ){
+                int num = entry.getKey();
+                int cnt = entry.getValue();
+
+                if(cnt > maxCount){
+                    maxCount = cnt;
+                    majority = num;
+                }
+            }
+
+            return majority;
+        }
+    }
+
+    public static void main(String[] args) {
+        int num[] = {1,1,1,2,2};
+
+        Solution sl = new Solution();
+        System.out.println('\n');
+        System.out.println(sl.majorityElement(num));
+
+    }
 }
