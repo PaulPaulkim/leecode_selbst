6a268d0b35e5fb9d15f8c3a4f78b82e1d447d357
paul kim <kth5009@gmail.com>
2025-08-29 14:23:46 +0200

    181 Employees Earning More Than Their Managers

--- sql/sql181.sql
+++ sql/sql181.sql
@@ -1,29 +1,22 @@
-CREATE DATABASE leetcode;
-USE leetcode;
+create database leecode181;
+use leecode181;
 
-create table Person(
-	PersonID INT,
-    FirstName varchar(255),
-    LastName varchar(255)
-);
+create table Employee(
+	id	int,
+    emp_name	varchar(255),
+    salary	int,
+    managerId	int
+    );
 
-create table Address(
-	AddressID INT,
-    PersonID INT,
-    City varchar(255),
-    State varchar(255)
-);
+insert into Employee values
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, null),
+(4, 'Max', 90000, null);
 
-insert into Person (PersonID, FirstName, LastName) values
-(1,'Allen','Wang'),
-(2, 'Bob','Alice');
+select e.emp_name as Employee
+from Employee as e
+Join Employee as m
+on	e.managerId = m.id
+where e.salary>m.salary;
 
-insert into Address (AddressID, PersonID, City, State) values
-(1,2,'New York City','New York'),
-(2, 3, 'Leetcode','California');
-
-
-select Person.FirstName, Person.LastName, Address.City, Address.State
-from Person
-left join Address
-	on Person.PersonID = Address.PersonID;
